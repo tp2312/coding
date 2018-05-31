26ad317222e317dc07c8730a60c0420bd3c96f02
zyh <1317197803@qq.com>
2018-05-31 14:03:08 +0800

    delete Hello.java

--- offer/xd/zyh/Hello.java
+++
@@ -1,9 +0,0 @@
-package xd.zyh
-
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		Systom.out.println("edit shark");
-		System.out.println("edit zyh");
-	}
-}
